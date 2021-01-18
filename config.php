54f21739bbddeba1b6d1fd15ba534138f1a9388c
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-01-18 17:35:26 +0530

    copied config.php with the dist version

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ *  wordpress-lti - WordPress module to add LTI support
+ *  Copyright (C) 2020  Simon Booth, Stephen P Vickers
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact: s.p.booth@stir.ac.uk
+ */
+
+use ceLTIc\LTI\Util;
+
+define('LTI_ID_SCOPE_DEFAULT', '3');
+define('LTI_LOG_LEVEL', Util::LOGLEVEL_ERROR);
+define('LTI_SIGNATURE_METHOD', 'RS256');
+define('LTI_KID', '');  // A random string to identify the key value
+define('LTI_PRIVATE_KEY', <<< EOD
+-----BEGIN RSA PRIVATE KEY-----
+...
+-----END RSA PRIVATE KEY-----
+EOD
+);
+
+###
+###  Registration settings
+###
+define('AUTO_ENABLE', false);
+define('ENABLE_FOR_DAYS', 0);
+?>
