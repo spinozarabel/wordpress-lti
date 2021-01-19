faa7a49404d98b8370b6ff7d1ed8e2d7d24e93a5
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-01-19 17:24:18 +0530

    Update config.php

--- config.php
+++ config.php
@@ -28,7 +28,19 @@ define('LTI_SIGNATURE_METHOD', 'RS256');
 define('LTI_KID', '');  // A random string to identify the key value
 define('LTI_PRIVATE_KEY', <<< EOD
 -----BEGIN RSA PRIVATE KEY-----
------
+MIICWwIBAAKBgGHnnqmt+3m2AxPpgU2Qeir8tAiOBIbfcQcure/cqyk+Z7nEaNPj
+frkBYnFgpOdhtv0dr4dK6BGOi2JT8VEdUOPLYjJRS94/2FPkVEtsjm0gKwnZEwl6
+LnS+Z0mfoyUJSDNfDf4QeglhAK1ctPsCsgyKafjMIFY9ovMp+d/HMBPHAgMBAAEC
+gYAA5196l4WTyQ9cNrKf4a6PCQgscAswp41mkJLAfRwDZHUWrO5+zkHUOWQMQeUj
+0a4bxhNhv1pHFzbIMJgwtIGTpccvtsLC4mVDN0Ev3I1WOyZeGug4KmFKVcdcXyEx
+04DczXC7dFXR67Y5qpoMTnKbvKKe+vnh81qtjW3hokhwyQJBAMNGrFfdjb2lT0WQ
+tHBq9KM06UJ11yE+a5zelFLdtY5BjhnYvm50wZWDiAAE0zmfSiWVvc6fR39E7zpo
+gHXPc1sCQQCAWYRN26KID6OAjPScAHEWuzPvwmb8gAYjx0T1ya+AqJQ3cPS1XnRs
+OG/ocf4bnyn10JW2eXjaB0+h8xdQf+kFAkEAtHz4AmZ3AdhvQp8TB+zznH3lM1Zz
+tvhYwq8/bLAbhRa2XtFkgfdMjgL6ivnquZGvGLokq3uwu8NdUiEQytMpjQJAcJMT
+cl60PfbJh9UaL0JL7o4fzamLPujjebosCBDwOD6kUcRnPjUslEckEJL7OCrwWMSs
+q7H7h/BlrjxTNK4cKQJAGdkakyQAuF+qJqun3XrrJa+H5kX0E4C2x+kPSdm+EIlR
+tmkuma2LS/ySzmsvjgn6CZFGV9An5Vg+1kagUmrCjQ==
 -----END RSA PRIVATE KEY-----
 EOD
 );
